0e145e869ddba44e19e5b7691b673779361f6185
Diogo Saldanha <diogossaldanha@hotmail.com>
2025-07-03 09:57:49 -0300

    Add Apache Commons Codec dependency and implement SHA-256 password hashing
    Fixes a NullPointerException when trying to validate credentials for nonexistent users. Hashing is now applied only after checking if the user exists in the map.

--- src/main/java/com/diogo/dev/Main.java
+++ src/main/java/com/diogo/dev/Main.java
@@ -1,5 +1,7 @@
 package com.diogo.dev;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -7,12 +9,15 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final Map<String, String> CREDENTIALS = new HashMap<>();
+    static {
+        CREDENTIALS.put("diogo", "senha123");
+        CREDENTIALS.put("robert", "password123");
+        CREDENTIALS.put("anoosh", "snuffles456");
+    }
+
     public static void main(String[] args) {
-        Map<String, String> credentials = new HashMap<>();
-        credentials.put("diogo", "senha123");
-        credentials.put("robert", "password123");
-        credentials.put("anoosh", "snuffles456");
-//        System.out.println(credentials);
+//        System.out.println(CREDENTIALS);
 //        System.out.println(credentials.get("diogo"));
 //        System.out.println(credentials.containsValue("diogo"));
 //        System.out.println(credentials.containsKey("diogo"));
@@ -25,7 +30,7 @@ public class Main {
         System.out.println("Digite uma senha: ");
         String password = scanner.nextLine();
 
-        if (isValidCredentials(username, password, credentials)) {
+        if (isValidCredentials(username, password)) {
             System.out.println("O GRÊMIO É O MAIOR TIME DO BRASIL");
         } else {
             System.out.println("Login e senha inválidos.");
@@ -33,10 +38,17 @@ public class Main {
 
     }
 
-    private static boolean isValidCredentials(String username, String password, Map<String, String> credentials) {
+    private static boolean isValidCredentials(String username, String password) {
 
-        return credentials.containsKey(username) && Objects.equals(credentials.get(username), password);
+        if (!CREDENTIALS.containsKey(username)) {
+            return false;
+        }
 
+        String sha256HashedPassword = DigestUtils.sha256Hex(password);
+        String storedUserPassword = CREDENTIALS.get(username);
+        String usernameHashedPassword = DigestUtils.sha256Hex(storedUserPassword);
+
+        return CREDENTIALS.containsKey(username) && Objects.equals(usernameHashedPassword, sha256HashedPassword);
     }
 
 }
